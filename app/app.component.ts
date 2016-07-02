291b8ce23f27915d21c9698c60e9d07ebf6a2dc7
Danish O'Habib <DohabiB@gmail.com>
2016-07-02 17:42:19 -0400

    creating demons

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,39 @@
 import {Component} from '@angular/core';
 
-@Component ({
+export class Demon {
+    id: number;
+    name : string;
+}
+
+
+@Component({
     selector: 'my-app',
-    template: '<h1> {{title}} </h1>' 
+    template: `
+    <h1> {{title}} {{demon.id}} {{demon.name}} </h1>
+    <h2>My Demons</h2>
+        <ul class = "demons">
+        <li *ngFor = 'let d of Demons'>
+            <span class = "badge">{{d.id}} {{d.name}}</span>
+        </li>
+        </ul>
+    `
 })
 
-export class AppComponent {  
-   title = 'Bestest Smartest App';
-   demons = 'Windstorm' ;
+export class AppComponent {
+    title = 'Bestest Smartest App';
+    demon: Demon = {
+        id: 1,
+        name : "Baal"
+    };
+    public Demons = DEMONS;
 }
+
+const DEMONS: Demon[] = [
+    {id: 12, name: 'Diablo'},
+    {id: 2, name:'Maphisto'},
+    {id: 3, name: 'Duriel'},
+    {id: 4, name: 'Butcher'}
+];
+
+
+
