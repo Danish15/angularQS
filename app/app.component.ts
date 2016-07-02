955160f366985fc848f1b4356cba4fab6be09c80
zaighumrajput <zaighum47@gmail.com>
2016-07-02 17:10:57 -0400

    title app

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,10 @@ import {Component} from '@angular/core';
 
 @Component ({
     selector: 'my-app',
-    template: '<h1>First Angular 2 App </h1>'
+    template: '<h1> {{title}} </h1>' 
 })
 
-export class AppComponent { }
+export class AppComponent {  
+   title = 'Bestest Smartest App';
+   demons = 'Windstorm' ;
+}
