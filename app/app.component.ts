99231b58d644a5b1c8ef400dfab419d121449b44
Danish O'Habib <DohabiB@gmail.com>
2016-07-03 18:23:08 -0400

    begginning sidebar

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,27 +2,29 @@ import {Component} from '@angular/core';
 
 import {Demon} from './demon';
 import {DemonDetailComponent} from './demon-detail.component';
+import {SideBarComponent} from './sidebar.component'
+
 @Component({
     selector: 'my-app',
-    templateUrl : 'app/app.component.html',
-    directives: [DemonDetailComponent]
-    })
+    templateUrl: 'app/app.component.html',
+    directives: [DemonDetailComponent, SideBarComponent]
+})
 
 
 export class AppComponent {
     title = 'Bestest Smartest App';
     selectedDemon: Demon;
     public Demons = DEMONS;
-    
-    onSelect (demon:Demon) {this.selectedDemon = demon;}
-    
+
+    onSelect(demon: Demon) { this.selectedDemon = demon; }
+
 }
 
 const DEMONS: Demon[] = [
-    {id: 12, name: 'Diablo'},
-    {id: 2, name:'Maphisto'},
-    {id: 3, name: 'Duriel'},
-    {id: 4, name: 'Butcher'}
+    { id: 12, name: 'Diablo' },
+    { id: 2, name: 'Maphisto' },
+    { id: 3, name: 'Duriel' },
+    { id: 4, name: 'Butcher' }
 ];
 
 
---
+++ app/sidebar.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core'
+
+
+@Component({
+    selector: 'my-sidebar',
+    templateUrl: 'app/sidebar.component.html'
+})
+
+
+export class SideBarComponent {
+    
+}
